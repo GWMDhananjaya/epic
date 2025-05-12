6578cb2aebe1be4aa0888e51f0be2f5baa8c4c3c
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2025-05-12 20:48:33 +0530

    first2 commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./components/state/store.js";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>
+  <BrowserRouter basename="/epic">
+    <StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </StrictMode>
+  </BrowserRouter>
 );
