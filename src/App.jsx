0c8ba681c508a70713c15412f75dbebeaf570c33
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2025-05-12 21:03:49 +0530

    first2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import AboutMeMain from "./components/aboutMeSection/AboutMeMain";
 import ContactMeMain from "./components/contactMeSection/ContactMeMain";
 import FooterMain from "./components/footer/FooterMain";
 import AboutGradient from "./components/aboutMeSection/AboutGradient";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ProjectsMain from "./components/projectImages/ProjectsMain";
 import ProjectGradient from "./components/projectImages/ProjectGradient";
 import ShopLocation from "./components/ShopLocation/ShopLocation";
