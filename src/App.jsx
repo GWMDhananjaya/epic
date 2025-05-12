817140ffbb5154ced460acf2939985d6aa1f655d
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2025-05-12 20:56:57 +0530

    first2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,35 @@ import AboutMeMain from "./components/aboutMeSection/AboutMeMain";
 import ContactMeMain from "./components/contactMeSection/ContactMeMain";
 import FooterMain from "./components/footer/FooterMain";
 import AboutGradient from "./components/aboutMeSection/AboutGradient";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import ProjectsMain from "./components/projectImages/ProjectsMain";
 import ProjectGradient from "./components/projectImages/ProjectGradient";
 import ShopLocation from "./components/ShopLocation/ShopLocation";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <main className="font-body">
-              <NavbarMain />
-              <HeroMain />
-              <HeroGradient />
-              <SubHeroSection />
-              <AboutMeMain />
-              <AboutGradient />
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <main className="font-body">
+            <NavbarMain />
+            <HeroMain />
+            <HeroGradient />
+            <SubHeroSection />
+            <AboutMeMain />
+            <AboutGradient />
 
-              <ProjectsMain />
-              <ProjectGradient />
-              <ShopLocation />
+            <ProjectsMain />
+            <ProjectGradient />
+            <ShopLocation />
 
-              <ContactMeMain />
-              <FooterMain />
-            </main>
-          }
-        />
-      </Routes>
-    </Router>
+            <ContactMeMain />
+            <FooterMain />
+          </main>
+        }
+      />
+    </Routes>
   );
 }
 
